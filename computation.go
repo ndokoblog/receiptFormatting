6d79094ef3586de10e7e3317908e37072d0c9659
Handoko Kustanto <>
2020-11-13 23:15:13 +0700

    sum string trim .00

--- computation.go
+++ computation.go
@@ -2,12 +2,13 @@ package receiptFormatting
 
 import (
 	"strconv"
+	"strings"
 )
 
 func SumStringNumber(sources []string) string {
 	sum := 0
 	for _, val := range sources {
-		num, _ := strconv.Atoi(val)
+		num, _ := strconv.Atoi(strings.TrimSuffix(val, ".00"))
 		sum += num
 	}
 	return strconv.Itoa(sum)
